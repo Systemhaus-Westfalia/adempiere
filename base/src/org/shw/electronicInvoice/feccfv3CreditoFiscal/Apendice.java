136cfedd0ea7a335041631e22c54487e70564446
SusanneCalderon <susanne.de.calderon@westfalia-it.com>
2023-09-02 11:46:55 -0600

    Electronic Invoice

---
+++ base/src/org/shw/electronicInvoice/feccfv3CreditoFiscal/Apendice.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.shw.electronicInvoice.feccfv3CreditoFiscal;
+
+/**
+ * 
+ */
+public class Apendice {
+	
+	String campo;
+	String etiqueta;
+	String valor;
+	
+	/**
+	 * No Parameters Actualizado	
+	 */
+	public Apendice() {
+
+	}
+
+	/**
+	 * @return the campo
+	 */
+	public String getCampo() {
+		return campo;
+	}
+
+	/**
+	 * @param campo the campo to set
+	 */
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+
+	/**
+	 * @return the etiqueta
+	 */
+	public String getEtiqueta() {
+		return etiqueta;
+	}
+
+	/**
+	 * @param etiqueta the etiqueta to set
+	 */
+	public void setEtiqueta(String etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public String getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

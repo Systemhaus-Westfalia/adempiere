b5b6b6992fac74651d04dc4a7528c311f805fea1
SusanneCalderon <susanne.de.calderon@westfalia-it.com>
2023-09-02 11:48:16 -0600

    Electronic Invoice

---
+++ base/src/org/shw/util/pojoAdempiere/TributosItem.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.shw.util.pojoAdempiere;
+
+/**
+ * 
+ */
+public class TributosItem {
+	
+	String codigo;
+	String descripcion;
+	String valor;
+	
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public String getValor() {
+		return valor;
+	}
+
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

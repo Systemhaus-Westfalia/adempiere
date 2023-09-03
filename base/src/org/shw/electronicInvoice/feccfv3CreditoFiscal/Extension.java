b550bfb16cd52737569991a20004a39ffdb44089
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-09-03 12:56:31 -0600

    Schema validation in class 'Extension'

--- base/src/org/shw/electronicInvoice/feccfv3CreditoFiscal/Extension.java
+++ base/src/org/shw/electronicInvoice/feccfv3CreditoFiscal/Extension.java
@@ -8,12 +8,12 @@ package org.shw.electronicInvoice.feccfv3CreditoFiscal;
  */
 public class Extension {
 	
-	String nombEntrega;
-	String docuEntrega;
-	String nombRecibe;
-	String docuRecibe;
-	String observaciones;
-	String placaVehiculo;
+	String nombEntrega=null;  // null allowed
+	String docuEntrega=null;  // null allowed
+	String nombRecibe=null;  // null allowed
+	String docuRecibe=null;  // null allowed
+	String observaciones=null;  // null allowed
+	String placaVehiculo=null;  // null allowed
 	
 	/**
 	 * No parameters
@@ -30,10 +30,19 @@ public class Extension {
 	}
 
 	/**
-	 * @param nombEntrega the nombEntrega to set
+	 * @param nombEntrega the nombEntrega to set<br>
+	 * The parameter is validated.<br>
+	 * "minLength" : 1, "maxLength" : 100; null also possible
 	 */
 	public void setNombEntrega(String nombEntrega) {
-		this.nombEntrega = nombEntrega;
+		final int MINLENGTH = 1;
+		final int MAXLENGTH = 100;
+		int length = nombEntrega.length();
+		
+		if( (length>=MINLENGTH && length<=MAXLENGTH) || (nombEntrega==null) )
+			this.nombEntrega = nombEntrega;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'nombEntrega' in setNombEntrega()");
 	}
 
 	/**
@@ -44,10 +53,19 @@ public class Extension {
 	}
 
 	/**
-	 * @param docuEntrega the docuEntrega to set
+	 * @param docuEntrega the docuEntrega to set<br>
+	 * The parameter is validated.<br>
+	 * "minLength" : 1, "maxLength" : 25; null also possible
 	 */
 	public void setDocuEntrega(String docuEntrega) {
-		this.docuEntrega = docuEntrega;
+		final int MINLENGTH = 1;
+		final int MAXLENGTH = 25;
+		int length = docuEntrega.length();
+		
+		if( (length>=MINLENGTH && length<=MAXLENGTH) || (docuEntrega==null) )
+			this.docuEntrega = docuEntrega;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'docuEntrega' in setDocuEntrega()");
 	}
 
 	/**
@@ -58,10 +76,19 @@ public class Extension {
 	}
 
 	/**
-	 * @param nombRecibe the nombRecibe to set
+	 * @param nombRecibe the nombRecibe to set<br>
+	 * The parameter is validated.<br>
+	 * "minLength" : 1, "maxLength" : 100; null also possible
 	 */
 	public void setNombRecibe(String nombRecibe) {
-		this.nombRecibe = nombRecibe;
+		final int MINLENGTH = 1;
+		final int MAXLENGTH = 100;
+		int length = nombRecibe.length();
+		
+		if( (length>=MINLENGTH && length<=MAXLENGTH) || (nombRecibe==null) )
+			this.nombRecibe = nombRecibe;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'nombRecibe' in setNombRecibe()");
 	}
 
 	/**
@@ -72,10 +99,19 @@ public class Extension {
 	}
 
 	/**
-	 * @param docuRecibe the docuRecibe to set
+	 * @param docuRecibe the docuRecibe to set<br>
+	 * The parameter is validated.<br>
+	 * "minLength" : 1, "maxLength" : 25; null also possible
 	 */
 	public void setDocuRecibe(String docuRecibe) {
-		this.docuRecibe = docuRecibe;
+		final int MINLENGTH = 1;
+		final int MAXLENGTH = 25;
+		int length = docuRecibe.length();
+		
+		if( (length>=MINLENGTH && length<=MAXLENGTH) || (docuRecibe==null) )
+			this.docuRecibe = docuRecibe;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'docuRecibe' in setDocuRecibe()");
 	}
 
 	/**
@@ -86,10 +122,18 @@ public class Extension {
 	}
 
 	/**
-	 * @param observaciones the observaciones to set
+	 * @param observaciones the observaciones to set<br>
+	 * The parameter is validated.<br>
+	 * "maxLength" : 3000; null also possible
 	 */
 	public void setObservaciones(String observaciones) {
-		this.observaciones = observaciones;
+		final int MAXLENGTH = 3000;
+		int length = observaciones.length();
+		
+		if( (length<=MAXLENGTH) || (observaciones==null) )
+			this.observaciones = observaciones;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'observaciones' in setObservaciones()");
 	}
 
 	/**
@@ -100,10 +144,19 @@ public class Extension {
 	}
 
 	/**
-	 * @param placaVehiculo the placaVehiculo to set
+	 * @param placaVehiculo the placaVehiculo to set<br>
+	 * The parameter is validated.<br>
+	 * "minLength" : 2, "maxLength" : 10; null also possible
 	 */
 	public void setPlacaVehiculo(String placaVehiculo) {
-		this.placaVehiculo = placaVehiculo;
+		final int MINLENGTH = 2;
+		final int MAXLENGTH = 10;
+		int length = placaVehiculo.length();
+		
+		if( (length>=MINLENGTH && length<=MAXLENGTH) || (placaVehiculo==null) )
+			this.placaVehiculo = placaVehiculo;
+		else
+	        throw new IllegalArgumentException("Wrong parameter 'placaVehiculo' in setPlacaVehiculo()");
 	}
 
 

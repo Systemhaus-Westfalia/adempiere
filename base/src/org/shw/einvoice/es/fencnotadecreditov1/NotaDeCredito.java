3b3c25d3bec36f6b4e8c50cf28fe6c79d43ce015
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-09-07 17:38:53 -0600

    Add package 'org.shw.einvoice.es.fencnotadecreditov1'. Add classes 'NotaDeCredito' and 'Identificacion' to package

---
+++ base/src/org/shw/einvoice/es/fencnotadecreditov1/NotaDeCredito.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.shw.einvoice.es.fencnotadecreditov1;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 
+ */
+public class NotaDeCredito {
+	
+	Identificacion identificacion;
+
+	/**
+	 * No parameters
+	 */
+	public NotaDeCredito() {
+		this.identificacion       = new Identificacion();
+	}
+
+	/**
+	 * @return the identificacion
+	 */
+	public Identificacion getIdentificacion() {
+		return identificacion;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}

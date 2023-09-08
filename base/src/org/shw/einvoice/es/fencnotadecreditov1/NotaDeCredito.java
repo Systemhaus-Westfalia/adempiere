adceb6fbf7779516592a749e8f1a10159c076714
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-09-08 14:08:17 -0600

    Add class 'Emisor' to 'NotaDeCredito'

--- base/src/org/shw/einvoice/es/fencnotadecreditov1/NotaDeCredito.java
+++ base/src/org/shw/einvoice/es/fencnotadecreditov1/NotaDeCredito.java
@@ -16,6 +16,7 @@ public class NotaDeCredito {
 	
 	Identificacion identificacion;
 	List<DocumentoRelacionadoItem> documentoRelacionado;
+	Emisor emisor;
 
 	/**
 	 * No parameters
@@ -23,6 +24,7 @@ public class NotaDeCredito {
 	public NotaDeCredito() {
 		this.identificacion       = new Identificacion();
 		this.documentoRelacionado = new ArrayList<DocumentoRelacionadoItem>();
+		this.emisor               = new Emisor();
 	}
 
 	/**
@@ -31,7 +33,42 @@ public class NotaDeCredito {
 	public Identificacion getIdentificacion() {
 		return identificacion;
 	}
-	
+
+	/**
+	 * @param identificacion the identificacion to set
+	 */
+	public void setIdentificacion(Identificacion identificacion) {
+		this.identificacion = identificacion;
+	}
+
+	/**
+	 * @return the documentoRelacionado
+	 */
+	public List<DocumentoRelacionadoItem> getDocumentoRelacionado() {
+		return documentoRelacionado;
+	}
+
+	/**
+	 * @param documentoRelacionado the documentoRelacionado to set
+	 */
+	public void setDocumentoRelacionado(List<DocumentoRelacionadoItem> documentoRelacionado) {
+		this.documentoRelacionado = documentoRelacionado;
+	}
+
+	/**
+	 * @return the emisor
+	 */
+	public Emisor getEmisor() {
+		return emisor;
+	}
+
+	/**
+	 * @param emisor the emisor to set
+	 */
+	public void setEmisor(Emisor emisor) {
+		this.emisor = emisor;
+	}
+
 	/**
 	 * @param args
 	 */

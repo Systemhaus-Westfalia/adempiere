af9d4dcb2fb73100922c4f5b16eafe0818eaa39a
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2023-09-12 11:34:25 -0600

    Add classes for Nota de Debito implementation

---
+++ base/src/org/shw/einvoice/es/fendnotadedebitov3/NotaDeDebito.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.shw.einvoice.es.fendnotadedebitov3;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.shw.einvoice.es.util.pojo.ApendiceItem;
+import org.shw.einvoice.es.util.pojo.Receptor;
+import org.shw.einvoice.es.util.pojo.VentaTercero;
+
+
+
+
+/**
+ * 
+ */
+public class NotaDeDebito {
+	
+	Identificacion identificacion;
+	List<DocumentoRelacionadoItem> documentoRelacionado;
+	Receptor receptor;
+	VentaTercero ventaTercero = null;
+	List<CuerpoDocumentoItem> cuerpoDocumento;
+	Resumen resumen;
+	Extension extension;
+	List<ApendiceItem> apendice=null;  // null allowed
+	//this.apendice             = new ArrayList<ApendiceItem>();
+
+	/**
+	 * No parameters
+	 */
+	public NotaDeDebito() {
+		this.identificacion       = new Identificacion();
+		this.documentoRelacionado = new ArrayList<DocumentoRelacionadoItem>();
+		this.receptor             = new Receptor();
+		//this.ventaTercero         = new VentaTercero();
+		this.cuerpoDocumento      = new ArrayList<CuerpoDocumentoItem>();
+		this.resumen              = new Resumen();
+		this.extension            = new Extension();
+	}
+
+	/**
+	 * @return the identificacion
+	 */
+	public Identificacion getIdentificacion() {
+		return identificacion;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
